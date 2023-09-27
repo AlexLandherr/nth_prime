4531c24fdd1933971868421d1f6d1ea317b7ff3a
Alex Landherr <alex.landherr@gmail.com>
2023-09-27 18:21:20 +0000

    Solved stated problem.

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,3 +1,49 @@
 #include "include/functions.h"
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+#include <stdexcept>
+#include <limits>
+#include <cstdint>
 
-namespace func {}
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we can see that the 6th prime is 13.
+What is the 10,001st prime number?
+*/
+
+namespace func {
+    bool is_prime(int64_t n) {
+        //Checks if a single signed 64-bit integer is a prime number.
+        if (n > std::numeric_limits<int64_t>::max() || n < std::numeric_limits<int64_t>::min()) {
+            throw std::invalid_argument("Argument 'n' is out of range for 'int64_t' type.");
+        }
+        if (n <= 1) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else if (n > 2 && n % 2 == 0) {
+            return false;
+        }
+
+        int64_t max_divisor = std::ceil(std::sqrt((long double) n));
+        for (int64_t d = 3; d < max_divisor + 1; d+=2) {
+            if (n % d == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    int64_t nth_prime(int64_t n) {
+        int prime_count = 1;
+        int64_t i = 2;
+        while (prime_count < n) {
+            i++;
+            if (is_prime(i)) {
+                prime_count++;
+            }
+        }
+        return i;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "include/functions.h"
 #include <iostream>
+#include <stdexcept>
 
 /*
 By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we can see that the 6th prime is 13.
@@ -7,6 +8,13 @@ What is the 10,001st prime number?
 */
 
 int main() {
+    try {
+        std::cout << "The 10,001st prime number is: " << func::nth_prime(10001) << '\n';
+    } catch (const std::invalid_argument& ex) {
+        std::cout << ex.what() << '\n';
+    } catch (const std::exception& ex) {
+        std::cout << ex.what() << '\n';
+    }
 
     return 0;
 }
